e5d6524351d4f44829491c36b099066e8c5b49dc
mahesh2291 <gmahesh252525@gmail.com>
2020-10-15 17:44:10 +0530

    navbar2 added

--- src/Components/Layout/Slider.js
+++ src/Components/Layout/Slider.js
@@ -1,27 +1,38 @@
-import Sider from 'antd/lib/layout/Sider';
 import React from 'react'
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Menu } from 'antd';
 
-function Slider() {
-    return (
-        <div>
-<Sider/>
-<Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-        <Menu.Item key="1">
-          nav 1
-        </Menu.Item>
-        <Menu.Item key="2" >
-          nav 2
-        </Menu.Item>
-        <Menu.Item key="3" >
-          nav 3
-        </Menu.Item>
-        <Menu.Item key="4" >
-          nav 4
-        </Menu.Item>
-      </Menu>
-        </div>
-    )
-}
 
-export default Slider
+
+
+
+class Sider extends React.Component {
+  
+  
+    render() {
+      return (
+        <Menu>
+          
+         
+              <Menu.Item key="1">Option 1</Menu.Item>
+              <Menu.Item key="2">Option 2</Menu.Item>
+            
+           
+              <Menu.Item key="3">Option 3</Menu.Item>
+              <Menu.Item key="4">Option 4</Menu.Item>
+           
+       
+         
+            <Menu.Item key="5">Option 5</Menu.Item>
+            <Menu.Item key="6">Option 6</Menu.Item>
+           
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+           
+       
+          
+        </Menu>
+      );
+    }
+  }
+
+export default Sider
